08cdd7a6f540326667d19516a84805a2fa242e0e
Jimmy <jimmy>
2020-09-01 22:16:42 +0800

    WIP

--- test/test_wider_face_data.py
+++ test/test_wider_face_data.py
@@ -1,12 +1,29 @@
 import cv2
+import numpy as np
 from bestvision.datasets.wider_face import WiderFace
+from bestvision.transforms.boxes import bbox2abox, abox2bbox
 from bestvision.utils.draw import draw_bboxes, draw_keypoints
+from bestvision.transforms import matrix2d, warp
+
+def transform(item):
+    bboxes = item.pop('bboxes')
+    aboxes = bbox2abox(bboxes)
+    image = item['image']
+    draw_bboxes(image, bboxes)
+    h, w = image.shape[:2]
+    matrix = matrix2d.hflip(w) @ matrix2d.center_rotate_scale_cw((w/2, h/2), 30, 1)
+    image = warp.affine(image, matrix, (w, h))
+    aboxes = matrix @ aboxes
+    item['image'] = image
+    item['bboxes'] = abox2bbox(aboxes)
+    return item
+
 
 if __name__ == "__main__":
     
     data = WiderFace(data_dir="F:\\sujz\\Data\\WIDER_FACE\\WIDER_train")
 
-    for item in data:
+    for item in data | transform:
         image = draw_bboxes(item['image'], item['bboxes'])
         draw_keypoints(image, item['keypoints'])
         cv2.imshow("v", image)
