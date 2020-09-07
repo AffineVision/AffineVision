81aacba0c407c786fc9774b3bdba94fcebc01d09
Jimmy <jimmy>
2020-09-07 23:20:03 +0800

    format

--- affinevision/transforms/boxes.py
+++ affinevision/transforms/boxes.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-# naming convention: 
+# naming convention:
 # bbox means bounding box encoded with bounding points
 # cbox means bounding box encoded with center and size
 # abox means affine box encoded as affine matrix
 #                                  [  u_x,   u_y, cx]
 #                                  [  v_x,   v_y, cy]
 #                                  [    0,     0,  1]
-# rbox means bounding rotated box encoding with [cx, cy, w, h, r] 
+# rbox means bounding rotated box encoding with [cx, cy, w, h, r]
 # where r is box rotated angle in radian, and the anchor is clockwise angle
 # in image coordinate
+
 
 def bbox2abox(bboxes, radians=None):
     # bboxes: [*, 4]
@@ -21,7 +22,8 @@ def bbox2abox(bboxes, radians=None):
     y_vec = vectors[..., 1]
     zeros = np.zeros(x_vec.shape)
     ones = np.ones(x_vec.shape)
-    aboxes = np.stack([x_vec, zeros, centers[..., 0], zeros, y_vec, centers[..., 1], zeros, zeros, ones], axis=-1)
+    aboxes = np.stack([x_vec, zeros, centers[..., 0], zeros,
+                       y_vec, centers[..., 1], zeros, zeros, ones], axis=-1)
     # reshape
     shape = (*x_vec.shape, 3, 3)
     aboxes = aboxes.reshape(shape)
@@ -29,10 +31,12 @@ def bbox2abox(bboxes, radians=None):
     if radians is not None:
         cos = np.cos(radians)
         sin = np.sin(radians)
-        rotate = np.stack([cos, -sin, zeros, sin, cos, zeros, zeros, zeros, ones], axis=-1).reshape(shape)
+        rotate = np.stack([cos, -sin, zeros, sin, cos, zeros,
+                           zeros, zeros, ones], axis=-1).reshape(shape)
         aboxes = rotate @ aboxes
 
     return aboxes
+
 
 def abox2bbox(aboxes):
     """covnert affine boxes to bounding point box
@@ -48,6 +52,7 @@ def abox2bbox(aboxes):
     bboxes = np.concatenate([c - e, c + e], axis=-1)
     return bboxes
 
+
 def bbox2cbox(bboxes):
     sizes = bboxes[..., 2:] - bboxes[..., :2]
     centers = (bboxes[..., :2] + bboxes[..., 2:]) / 2
@@ -62,9 +67,11 @@ def cbox2bbox(bboxes):
     bboxes = np.concatenate([lt, rb], axis=-1)
     return bboxes
 
+
 def cbox2abox(cboxes):
     bboxes = cbox2bbox(cboxes)
     return bbox2abox(bboxes)
+
 
 def bbox_affine(bboxes, matrix):
     aboxes = bbox2abox(bboxes)
